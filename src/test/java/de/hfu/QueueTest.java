feff4021825186226b58666b3e017c1cedefc123
Sascha Schettler <sascha.schettler@hs-furtwangen.de>
2020-06-17 18:01:31 +0200

    changed tests

--- src/test/java/de/hfu/QueueTest.java
+++ src/test/java/de/hfu/QueueTest.java
@@ -5,21 +5,39 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class QueueTest {
+    int queueSize = 3;
+    Queue queue = new Queue(queueSize);
 
     @Test
     public void enqueue() {
-        int queueSize = 3;
-        Queue queue = new Queue(queueSize);
-        for(int i=0;i<queueSize;++i){
+        for (int i = 0; i < queueSize; ++i) {
             queue.enqueue(i);
         }
 
-        for(int i=0;i<queueSize;++i){
+        for (int i = 0; i < queueSize; ++i) {
             assertEquals(i, queue.dequeue());
         }
     }
 
-    @Test
-    public void dequeue() {
+    /*
+    Test ungültige Queuelänge
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidLength() {
+        Queue queue = new Queue(-1);
+        Queue queue2 = new Queue(0);
+    }
+
+    /*
+    Test leere Queue
+     */
+    @Test(expected = IllegalStateException.class)
+    public void emptyQueue() {
+        for (int i = 0; i < queueSize; ++i) {
+            queue.enqueue(i);
+        }
+        for (int i = 0; i < queueSize + 2; ++i) {
+            assertEquals(i, queue.dequeue());
+        }
     }
 }

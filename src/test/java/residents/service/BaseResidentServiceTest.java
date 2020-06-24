8e7c99def1e7cabbe3535cdb7c7859bbeca932ac
Sascha Schettler <sascha.schettler@hs-furtwangen.de>
2020-06-24 11:44:25 +0200

    changed data for Aufgabe 7

---
+++ src/test/java/residents/service/BaseResidentServiceTest.java
@@ -0,0 +1,15 @@
+package residents.service;
+
+import static org.junit.Assert.*;
+import residents.repository.ResidentRepository;
+import residents.service.BaseResidentService;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matcher.*;
+
+public class BaseResidentServiceTest {
+
+    public void testTransfer(){
+
+    }
+
+}

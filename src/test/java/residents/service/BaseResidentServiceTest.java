b77e894bdbd58b82bbd78c1767857c0e1e07bb83
Sascha Schettler <sascha.schettler@hs-furtwangen.de>
2020-06-24 16:27:32 +0200

    changed files for Aufgabe 7

---
+++ src/test/java/residents/service/BaseResidentServiceTest.java
@@ -0,0 +1,10 @@
+package residents.service;
+
+import residents.domain.Resident;
+import residents.repository.ResidentRepositoryStub;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class BaseResidentServiceTest {
+
+}
